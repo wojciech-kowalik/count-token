5cd71f5a715a91f3674fda4177e0f21240bcd99c
Wojciech Kowalik <wkowalik81@gmail.com>
2023-07-06 15:23:17 +0200

    chore: new readme and error handling

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# count-token function
+# Count GPT3 token function on Netlify
+
+[![Deploy to
+Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/wojciech-kowalik/count-token)
+
+
+```sh
+curl https://count-token.netlify.app/.netlify/functions/count
+curl --header "Content-Type: application/json" --request POST --data '{"text":"test"}' https://count-token.netlify.app/.netlify/functions/count
+```
--- netlify/functions/count/index.js
+++ netlify/functions/count/index.js
@@ -1,51 +1,37 @@
-// exports.handler = async (event, context) => {
-//   if (event.httpMethod === "POST") {
-//     try {
-//       const payload = JSON.parse(event.body);
-//       const text = payload.text;
-//       return {
-//         statusCode: 200,
-//         body: JSON.stringify({
-//           tokens: encode(text).length,
-//         }),
-//       };
-//     } catch (error) {
-//       return {
-//         statusCode: 500,
-//         body: JSON.stringify({ error: error.message }),
-//       };
-//     }
-//   } else {
-//     return {
-//       statusCode: 501,
-//       body: JSON.stringify({ message: "Not Implemented" }),
-//       headers: { "content-type": "application/json" },
-//     };
-//   }
-// };
-
 "use strict";
+
 const express = require("express");
 const serverless = require("serverless-http");
-const app = express();
 const bodyParser = require("body-parser");
 const { encode } = require("gpt-3-encoder");
 
+const app = express();
 const router = express.Router();
-
-router.post("/", (request, response) => {
- const payload = JSON.parse(request.body);
- const text = payload.text;
-
- return response.json({
-   body: JSON.stringify({
-     tokens: encode(text).length,
-   }),
- });
-})
 
 app.use(bodyParser.json());
 app.use("/.netlify/functions/count", router);
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err,
+  });
+});
+
+router.post("/", (request, response) => {
+  const text = request.body?.text;
+
+  if (!text) {
+    throw Error("No 'text' provided.");
+  }
+
+  return response.json({
+    tokens: encode(text).length,
+  });
+});
 
 module.exports = app;
 module.exports.handler = serverless(app);
